e1780e87b3fb5a19fe1237a788c888483938b355
Erin Sheridan <SheridanErin@gmail.com>
2016-02-10 15:23:58 -0700

    completed

--- instanceArray.js
+++ instanceArray.js
@@ -14,28 +14,39 @@ var User = function(name, email, pw){
 //Create an Array called 'users' that will store all our instances of User.
 
   //code here
-
+var users = [];
 
 //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
 
   //code here
+users.push(new User('Tyler', 'tylermcginnis33@gmail.com', 'iLoveJS'));
+users.push(new User('Josh', 'cahlan@devmounta.in', 'iLoveHashtags'));
+users.push(new User('Lenny', 'lenny@theLenster.com', 'iLoveLentilSoup'));
+
+// users.push(new User('Erin', 'SheridanErin@gmail.com', 'bleh'));
+// users.push(new User('Josh', 'ReactDude@gmail.com', 'blah'));
+// users.push(new User('Scott', 'AngryGinger@gmail.com', 'bluh'));
 
 console.log('Tyler\'s information is ');
 //Console.log all of Tylers information
 
   //code here
+console.log(users[0]);
 
 console.log('Lenny\'s information is ');
 //Now console.log all of Lennys information
 
   //code here
-
+console.log(users[2]);
 
 //Now create another instance of User using your own information and then add that to your users array.
-
+users.push(new User('Erin', 'SheridanErin@gmail.com', 'bleh'));
   //code here
 
 console.log('All my users names are ');
 //Now loop through your users Array and console.log every users name. 
 
   //code here
+users.forEach(function(user) {
+  console.log(user);
+})
--- quizApp.js
+++ quizApp.js
@@ -7,26 +7,39 @@
 
   //code here
 
+function QuizUser(name, email, password, totalScore) {
+	this.name = name;
+	this.email = email;
+	this.password = password;
+	this.totalScore = totalScore;
+}
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty parameters
 
   //code here
-
+function Question(title, answersArray, rightAnswer, difficulty) {
+	this.title = title;
+	this.answersArray = answersArray;
+	this.rightAnswer = rightAnswer;
+	this.difficulty = difficulty;
+}
 
 //Create a quizUsers Array which is going to hold all of our users.
 
   //code here
-
+var quizUsers = [];
 
 //Let's say three people signed up for our service, create 3 instances of User and add each to the users Array
 
   //code here
-
+quizUsers.push(new User('Erin', 'sheridanerin@gmail.com', 'bleh', 10));
+quizUsers.push(new User('Josh', 'ReactDude@gmail.com', 'blah', 9));
+quizUsers.push(new User('Scott', 'AngryGinger@gmail.com', 'bluh', 8));
 
 //Create a questions Array which is going to hold all of our questions
 
   //code here
-
+var questions = [];
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
 //title: 'T/F: Inheritance is achieved in JavaScript through Prototypes?'
@@ -35,11 +48,16 @@
 //Fill in the rest of the required data as you see appropriate.
 
   //code here
-
+var question1 = new Question('T/F: Inheritance is achieved in JavaScript through Prototypes?', [], 'true', 2);
+var question2 = new Question('T/F: JavaScript is just a scripting version of Java', [], 'true', 2);
+var question3 = new Question("T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value", [], 'true', 2);
 
 //Now push all of your instances of Question into the questions Array
 
   //code here
+  questions.push(question1);
+  questions.push(question2);
+  questions.push(question3);
 
 console.log('My users Array and my questions arrray are ...');
 //Now loop console.log your users array and your questions array and verify that they're both holding the right data.
